f0e670ea89cfa65368d4a5384f435603ac18a98c
Vitaliy Karnienko <karnienko@yandex-team.ru>
2018-03-16 20:35:41 +0300

    Initial commit

---
+++ Alice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alice
+//
+//  Created by Vitaliy Karnienko on 16.03.2018.
+//  Copyright © 2018 Vitaliy Karnienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alice
+//
+//  Created by Vitaliy Karnienko on 16.03.2018.
+//  Copyright © 2018 Vitaliy Karnienko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
